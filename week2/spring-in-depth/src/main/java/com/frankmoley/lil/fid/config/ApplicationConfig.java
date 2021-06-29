5f676e26ac17886470dac98d77c67690d260f2cd
Paola Pileno <paola.pileno@wizeline.com>
2021-06-28 21:21:54 -0500

    Aspects in Spring

--- week2/spring-in-depth/src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
+++ week2/spring-in-depth/src/main/java/com/frankmoley/lil/fid/config/ApplicationConfig.java
@@ -5,50 +5,15 @@
 
 package com.frankmoley.lil.fid.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
-
-import com.frankmoley.lil.fid.service.GreetingService;
-import com.frankmoley.lil.fid.service.OutputService;
-import com.frankmoley.lil.fid.service.TimeService;
 
 @Configurable
 @PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.frankmoley.lil.fid")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
-
-    @Value("${app.greeting}")
-    private String greeting;
-
-    @Value("${app.name}")
-    private String name;
-
-    @Value("#{new Boolean(environment['spring.profiles.active'] != 'dev')}")
-    private boolean is24;
-
-    @Autowired
-    private GreetingService greetingService;
-
-    @Autowired
-    private TimeService timeService;
-
-    @Bean
-    public TimeService timeService() {
-        return new TimeService(is24);
-    }
-
-    @Bean
-    public OutputService outputService() {
-        return new OutputService(greetingService, timeService, name);
-    }
-
-    @Bean
-    public GreetingService greetingService() {
-        return new GreetingService(greeting);
-    }
-
 
 }
