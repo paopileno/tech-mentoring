2c31b14baca0e4a18519012742cf3c608cc6e84b
Paola Pileno <paola.pileno@wizeline.com>
2021-07-21 21:12:50 -0500

    (week 4) Chapter 1, 2  and 3

---
+++ week4/explorecali/src/main/java/com/wizeline/techmentoring/explorecali/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.wizeline.techmentoring.explorecali.domain;
+
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
---
+++ week4/explorecali/src/main/java/com/wizeline/techmentoring/explorecali/service/TourPackageService.java
@@ -0,0 +1,32 @@
+package com.wizeline.techmentoring.explorecali.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wizeline.techmentoring.explorecali.domain.TourPackage;
+import com.wizeline.techmentoring.explorecali.repository.TourPackageRepository;
+
+@Service
+public class TourPackageService {
+    private final TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourPackageService(TourPackageRepository tourPackageRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    public TourPackage createTourPackage(String code, String name) {
+        return tourPackageRepository.findById(code).orElse(
+                tourPackageRepository.save(new TourPackage(code, name))
+        );
+    }
+
+    public Iterable<TourPackage> lookup() {
+        return tourPackageRepository.findAll();
+    }
+
+    public long total() {
+        return tourPackageRepository.count();
+    }
+
+}
